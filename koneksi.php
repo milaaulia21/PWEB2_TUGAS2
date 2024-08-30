b79d5bb0f300c6f4bf9ff862262c6406e75d25de
milaaulia21 <165994151+milaaulia21@users.noreply.github.com>
2024-08-30 11:02:13 +0700

    Update koneksi.php

--- koneksi.php
+++ koneksi.php
@@ -1,20 +1,25 @@
 <?php
+// Kelas ini menangani koneksi ke database
 class Database {
+// Atribut private untuk menyimpan informasi koneksi database
     private $host = "localhost";
     private $username = "root";
     private $password = "";
     private $database = "pweb2_tugas2";
     protected $koneksi = "";
-
+// Constructor untuk membuat koneksi ke database
     function __construct() 
     {
+	// Membuat koneksi ke database menggunakan mysqli_connect
        $this->koneksi = mysqli_connect($this->host, $this->username, $this->password, $this->database);
-       if (mysqli_connect_errno()) {
+  	// Mengecek apakah koneksi berhasil atau gagal
+	if (mysqli_connect_errno()) {
+	// Menampilkan pesan kesalahan jika koneksi gagal
         echo "Koneksi database gagal : " . mysqli_connect_error();
        }
     }
 } 
-
+// Metode Menampilkan data Students
 function tampil_data_students()
 	{
 		$data = mysqli_query($this->koneksi,"SELECT * FROM student");
@@ -24,6 +29,7 @@ function tampil_data_students()
 		return $hasil;
 	}
 
+// Metode Menampilkan data Achievements
     function tampil_data_achievements()
 	{
 		$data = mysqli_query($this->koneksi,"SELECT * FROM achievements");
@@ -32,29 +38,30 @@ function tampil_data_students()
 		}
 		return $hasil;
 	}
-
+// Method untuk mengambil data dari tabel student
 class Students extends Database {
 
     function tampil_data_students(){
-        $query = "SELECT * FROM student";
-        $data = mysqli_query($this->koneksi, $query);
-        while($row = mysqli_fetch_array($data)){
-			$hasil[] = $row;
+        $query = "SELECT * FROM student"; // Query SQL untuk memilih semua data dari tabel student
+        $data = mysqli_query($this->koneksi, $query); // Eksekusi query
+        while($row = mysqli_fetch_array($data)){ // Mengambil hasil query dalam bentuk array
+			$hasil[] = $row; // Menyimpan setiap baris data ke array hasil
 		}
-		return $hasil;
+		return $hasil;  // Mengembalikan array hasil
 
     }
 }
 
 class Achievements extends Database {
-    
+    // Method untuk mengambil data dari tabel achievements
     function tampil_data_achievements(){
-        $query = "SELECT * FROM achievements";
-        $data = mysqli_query($this->koneksi, $query);
-        while($row = mysqli_fetch_array($data)){
-			$hasil[] = $row;
+        $query = "SELECT * FROM achievements"; // Query SQL untuk memilih semua data dari tabel achievements
+        $data = mysqli_query($this->koneksi, $query); // Eksekusi query
+        while($row = mysqli_fetch_array($data)){ // Mengambil hasil query dalam bentuk array
+			$hasil[] = $row; // Menyimpan setiap baris data ke array hasil
 		}
-		return $hasil;
+		return $hasil; // Mengembalikan array hasil
+
 
     }
 }
